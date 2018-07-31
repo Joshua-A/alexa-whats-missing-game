674122a8e96026a3c2af2b3ba1149ccbf5bebd31
Josh Adams <joshua@cj2o.com>
2018-07-31 19:33:30 +0100

    added handlers to skill builder

--- index.js
+++ index.js
@@ -8,10 +8,18 @@ const Alexa = require('ask-sdk-core');
 
 /* LABELS */
 
-const msgWelcome = "Welcome to What's Missing!\
-                    I'm going to list the items I have here with me,\
-                    then shuffle them up and remove one item. It will be your job to tell me\
-                    which item I have removed. Are you ready?";
+const WELCOME_MESSAGE = 
+'Welcome to What\'s Missing! \
+I\'m going to list the items I have here with me, \
+then shuffle them up and remove one item. It will be your job to tell me \
+which item I have removed. Are you ready?';
+const READY_MESSAGE = 'Are you ready to play?';
+const STARTNEW_MESSAGES = [
+    '<say-as interpret-as="interjection">okey dokey</say-as>, here we go.',
+    'Okay then, let\'s begin.',
+    'Alright then, off we go.'
+];
+
 
 
 /* HANDLERS */
@@ -23,35 +31,76 @@ const LaunchRequestHandler = {
         return request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
-
-        return handlerInput.responseBuilder.speak(msgWelcome).getResponse();
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        attributes.stage = 'start'; // Initialise the session stage var
+        handlerInput.attributesManager.setSessionAttributes(attributes);
+        return handlerInput.responseBuilder
+            .speak(WELCOME_MESSAGE)
+            .reprompt(READY_MESSAGE)
+            .getResponse();
     }
 }
 
 // When a user tries to start a new game at the start or end
 const StartGameHandler = {
     canHandle(handlerInput) {
-
+        const request = handlerInput.requestEnvelope.request;
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        return request.type === 'IntentRequest' &&
+            (request.intent.name === "StartGameIntent" ||
+            request.intent.name === "AMAZON.StartOverIntent" ||
+            request.intent.name === "AMAZON.YesIntent") &&
+            attributes.stage !== 'playing';
     },
     handle(handlerInput) {
-
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        attributes.stage = 'playing';
+        handlerInput.attributesManager.setSessionAttributes(attributes);
+        return handlerInput.responseBuilder
+            .speak(pickRandomLabel(STARTNEW_MESSAGES))
+            .getResponse();
     }
 }
 
 // When a user tries to start a new game and one is in progress
 const RestartGameHandler = {
     canHandle(handlerInput) {
-
+        const request = handlerInput.requestEnvelope.request;
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        return request.type === 'IntentRequest' &&
+            (request.intent.name === 'StartGameIntent' ||
+            request.intent.name === 'AMAZON.StartOverIntent') &&
+            attributes.stage === 'playing';
     },
     handle(handlerInput) {
 
     }
 }
 
-// When a userprovides an answer
+// When a user provides an answer
 const AnswerHandler = {
     canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        return request.type === 'IntentRequest' &&
+            request.intent.name === 'AnswerIntent' &&
+            attributes.stage === 'playing';
+    },
+    handle(handlerInput) {
 
+    }
+}
+
+// When a user says no to a new game or chooses to exit
+const QuitHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        return request.type === 'IntentRequest' &&
+            (request.intent.name === 'AMAZON.NoIntent' ||
+            request.intent.name === 'AMAZON.StopIntent' ||
+            request.intent.name === 'AMAZON.CancelIntent') &&
+            attributes.stage === 'playing';
     },
     handle(handlerInput) {
 
@@ -61,7 +110,11 @@ const AnswerHandler = {
 /* Skills builder */
 const skillBuilder = Alexa.SkillBuilders.custom();
 exports.handler = skillBuilder.addRequestHandlers(
-        LaunchRequestHandler
+        LaunchRequestHandler,
+        StartGameHandler,
+        RestartGameHandler,
+        AnswerHandler,
+        QuitHandler
     ).lambda();
 
 
